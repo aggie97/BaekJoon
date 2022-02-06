7b9c3201faab1648c18e7681c0e62c72ce2a70ac
aggie <4ggie1997@gmail.com>
2022-02-06 23:54:51 +0900

    BaekJoon 4344

---
+++ 4344.js
@@ -0,0 +1,15 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const testCase = parseInt(input[0]);
+
+for(let i = 1; i <= testCase; i++){
+  let studentNum = parseInt(input[i][0]);
+  let sum = 0;
+  for(let j = 1; j <= studentNum; j++){
+    sum += parseInt(input[i].split(' ')[j]);
+  }
+  let avg = sum / studentNum;
+  let smart = input[i].split(' ').filter((score) => Number(score) > avg);
+  console.log(smart);
+  console.log((smart.length / studentNum * 100).toFixed(3)+"%");
+}
