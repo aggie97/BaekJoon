72a1fd0d29d1ecc4d4b236d4ddb4930988455b5e
aggie <4ggie1997@gmail.com>
2022-03-02 14:26:23 +0900

    BakeJoon 10757

---
+++ 10757.js
@@ -0,0 +1,8 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+
+const A = BigInt(input[0]);
+const B = BigInt(input[1]);
+
+const result = A + B;
+
+console.log(String(result));
